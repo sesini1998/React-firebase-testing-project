9ee2261680a8bcd3b81d140cd9534f3ac7fec81c
Sesini <navodyaaluthdurage@gmail.com>
2023-11-27 08:17:14 +0530

    Connection between React & Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+// import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import 'firebase/database';
+import {getDatabase , ref , get , push , onValue} from "firebase/database";
 
-function App() {
+const firebaseConfig = {
+    apiKey: "AIzaSyAXR0sh_vf6yfRMcz_jBGQnXWIva8uFeg0",
+    authDomain: "ecg-signal-data.firebaseapp.com",
+    databaseURL: "https://ecg-signal-data-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ecg-signal-data",
+    storageBucket: "ecg-signal-data.appspot.com",
+    messagingSenderId: "341514934731",
+    appId: "1:341514934731:web:9ea980f7539aa260543e4e",
+    measurementId: "G-927HBPEEWH"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+const Test = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const databaseRef = ref(database ,'sample');
+    console.log(database);
+    // const fetchData = () => {
+    //   databaseRef.on('value', (snapshot) => {
+    //     const items = snapshot.val();
+    //     const itemsArray = items ? Object.values(items) : [];
+    //     setData(itemsArray);
+    //   });
+    // };
+
+    // fetchData();
+
+    // return () => {
+    //   databaseRef.off('value');
+    // };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Firebase Realtime Database Example</h1>
+      {/* <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            {`Item ID: ${item.id}, Name: ${item.name}`}
+          </li>
+        ))}
+      </ul> */}
     </div>
   );
-}
+};
 
-export default App;
+export default Test;
